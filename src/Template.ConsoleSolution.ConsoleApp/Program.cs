07aa25a7456ad5af6a7d9a2894a4ac70816cadf0
Gary Jackson <garyljackson@gmail.com>
2020-02-25 14:37:46 +1000

    Initial Commit

---
+++ src/Template.ConsoleSolution.ConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Template.ConsoleSolution.ConsoleApp
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
