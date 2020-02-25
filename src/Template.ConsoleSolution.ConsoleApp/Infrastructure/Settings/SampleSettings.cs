b743c85ecbbb285aeb94f587a79b95e037272fc6
Gary Jackson <garyljackson@gmail.com>
2020-02-25 15:53:48 +1000

    Add sample app settings

---
+++ src/Template.ConsoleSolution.ConsoleApp/Infrastructure/Settings/SampleSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace Template.ConsoleSolution.ConsoleApp.Infrastructure.Settings
+{
+    internal class SampleSettings
+    {
+        public static string ConfigSettingName = "Sample";
+        public string Setting1 { get; set; } = string.Empty;
+        public string Setting2 { get; set; } = string.Empty;
+    }
+}
