2d3b1a69c951decfa022e0efa5c639967c05e78b
Gary Jackson <garyljackson@gmail.com>
2020-02-25 15:54:14 +1000

    Add sample feature

---
+++ src/Template.ConsoleSolution.ConsoleApp/Features/SampleFeature/IMySampleFeature.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace Template.ConsoleSolution.ConsoleApp.Features.SampleFeature
+{
+    public interface IMySampleFeature
+    {
+        Task DoFeatureWorkAsync();
+    }
+}
---
+++ src/Template.ConsoleSolution.ConsoleApp/Features/SampleFeature/MySampleFeature.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Options;
+using Template.ConsoleSolution.ConsoleApp.Infrastructure.Settings;
+
+namespace Template.ConsoleSolution.ConsoleApp.Features.SampleFeature
+{
+    internal class MySampleFeature : IMySampleFeature
+    {
+        private readonly IOptions<SampleSettings> _sampleSettings;
+
+        public MySampleFeature(IOptions<SampleSettings> sampleSettings)
+        {
+            _sampleSettings = sampleSettings;
+        }
+
+        public Task DoFeatureWorkAsync()
+        {
+            Console.WriteLine($"Setting 1 (appsettings.json): {_sampleSettings.Value.Setting1}");
+            Console.WriteLine($"Setting 2 (appsettings.Development.json): {_sampleSettings.Value.Setting2}");
+            return Task.CompletedTask;
+        }
+    }
+}
